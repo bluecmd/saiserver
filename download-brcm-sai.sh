84dd13ff2f9f32d0e85bbf84df24570f0a6f87f4
Christian Svensson <blue@cmd.nu>
2025-07-15 19:33:38 +0200

    Update SAI download URLs

--- download-brcm-sai.sh
+++ download-brcm-sai.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <switch to grab SAI version from>"
+  exit 1
+fi
+
 echo ' [.] Searching for BRCM version artifacts...'
 
-BRANCH="$(docker exec syncd cat /usr/lib/libsai.so.1.0 | strings | grep -m1 -oE 'SAI_[0-9]+\.[0-9]+\.[0-9]+_GA')"
-VERSION="$(docker exec syncd cat /usr/lib/libsai.so.1.0 | strings | grep -m1 'BRCM SAI ver:' -C 1000 | grep -m1 -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$')"
+BRANCH="$(ssh "$1" docker exec syncd cat /usr/lib/libsai.so.1.0 | strings | grep -m1 -oE 'SAI_[0-9]+\.[0-9]+\.[0-9]+_GA')"
+VERSION="$(ssh "$1" docker exec syncd cat /usr/lib/libsai.so.1.0 | strings | grep -m1 'BRCM SAI ver:' -C 1000 | grep -m1 -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$')"
 
 set -euo pipefail
 
@@ -14,5 +19,5 @@ fi
 
 echo " [*] Detected BRCM SAI branch ${BRANCH}, version ${VERSION}"
 
-curl -L -O "https://sonicstorage.blob.core.windows.net/public/sai/sai-broadcom/${BRANCH}/${VERSION}/xgs/libsaibcm_${VERSION}_amd64.deb"
-curl -L -O "https://sonicstorage.blob.core.windows.net/public/sai/sai-broadcom/${BRANCH}/${VERSION}/xgs/libsaibcm-dev_${VERSION}_amd64.deb"
+curl -L -O "https://packages.trafficmanager.net/public/sai/sai-broadcom/${BRANCH}/${VERSION}/xgs/libsaibcm_${VERSION}_amd64.deb"
+curl -L -O "https://packages.trafficmanager.net/public/sai/sai-broadcom/${BRANCH}/${VERSION}/xgs/libsaibcm-dev_${VERSION}_amd64.deb"
